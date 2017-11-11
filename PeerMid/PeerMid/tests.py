7032f37d8a108c9f9080168ff6e3ba98f624dbc3
Woojgh <jamessalamonsen@gmail.com>
2017-11-11 11:24:42 -0800

    adding in cats

--- PeerMid/PeerMid/tests.py
+++ PeerMid/PeerMid/tests.py
@@ -1,65 +1,24 @@
-import unittest
-import transaction
-
 from pyramid import testing
+from pyramid.response import Response
 
 
-def dummy_request(dbsession):
-    return testing.DummyRequest(dbsession=dbsession)
+def test_home_view_returns_response():
+    """Home view returns a Response object."""
+    from expense_tracker.views.default import home_page
+    request = testing.DummyRequest()
+    response = home_page(request)
+    assert isinstance(response, Response)
 
+def test_home_view_is_good():
+    """Home view response has a status 200 OK."""
+    from expense_tracker.views.default import home_page
+    request = testing.DummyRequest()
+    response = home_page(request)
+    assert response.status_code == 200
 
-class BaseTest(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp(settings={
-            'sqlalchemy.url': 'sqlite:///:memory:'
-        })
-        self.config.include('.models')
-        settings = self.config.get_settings()
-
-        from .models import (
-            get_engine,
-            get_session_factory,
-            get_tm_session,
-            )
-
-        self.engine = get_engine(settings)
-        session_factory = get_session_factory(self.engine)
-
-        self.session = get_tm_session(session_factory, transaction.manager)
-
-    def init_database(self):
-        from .models.meta import Base
-        Base.metadata.create_all(self.engine)
-
-    def tearDown(self):
-        from .models.meta import Base
-
-        testing.tearDown()
-        transaction.abort()
-        Base.metadata.drop_all(self.engine)
-
-
-class TestMyViewSuccessCondition(BaseTest):
-
-    def setUp(self):
-        super(TestMyViewSuccessCondition, self).setUp()
-        self.init_database()
-
-        from .models import MyModel
-
-        model = MyModel(name='one', value=55)
-        self.session.add(model)
-
-    def test_passing_view(self):
-        from .views.default import my_view
-        info = my_view(dummy_request(self.session))
-        self.assertEqual(info['one'].name, 'one')
-        self.assertEqual(info['project'], 'PeerMid')
-
-
-class TestMyViewFailureCondition(BaseTest):
-
-    def test_failing_view(self):
-        from .views.default import my_view
-        info = my_view(dummy_request(self.session))
-        self.assertEqual(info.status_int, 500)
+def test_home_view_returns_proper_content():
+    """Home view response has html content."""
+    from expense_tracker.views.default import home_page
+    request = testing.DummyRequest()
+    response = home_page(request)
+    assert "Snorgle Cats" in response.text
--- PeerMid/PeerMid/views/default.py
+++ PeerMid/PeerMid/views/default.py
@@ -1,6 +1,10 @@
 from pyramid.response import Response
+import io
+import os
 
+HERE = os.path.dirname(__file__)
 
 def home_page(request):
-    """View page for the Home ways"""
-    return Response("This is my dumb view")
+    imported_text = io.open(os.path.join(HERE, 'home.html')).read()
+    return Response(imported_text)
+
