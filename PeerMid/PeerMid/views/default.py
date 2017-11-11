0b17966b8bc67cab8fe73bbfa7344afcd6ecf381
Woojgh <jamessalamonsen@gmail.com>
2017-11-11 12:13:33 -0800

    adding anorgle cats

--- PeerMid/PeerMid/views/default.py
+++ PeerMid/PeerMid/views/default.py
@@ -1,4 +1,6 @@
 from pyramid.response import Response
+from PeerMid.data.cats import CATS
+from pyramid.view import view_config
 import io
 import os
 
@@ -8,3 +10,7 @@ def home_page(request):
     imported_text = io.open(os.path.join(HERE, 'home.html')).read()
     return Response(imported_text)
 
+@view_config(route_name='home', renderer='../templates/cat_list.jinja2')
+def list_view(request):
+    """View for listing cats."""
+    return {"cats": CATS}
